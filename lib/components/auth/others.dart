5ab0519f460efa40056e8bc888d2778722b97e68
Ali Nawaz <131615374+Ali01129@users.noreply.github.com>
2025-03-26 01:36:35 +0500

    login page has some height and scrollable errors

---
+++ lib/components/auth/others.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+
+class Others extends StatelessWidget {
+  const Others({super.key});
+
+  bool isDarkMode(BuildContext context) {
+    return Theme.of(context).brightness == Brightness.dark;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    bool darkMode = isDarkMode(context);
+
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 4.w),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4.w),
+              border: Border.all(
+                width: 2,
+                color: darkMode ? Color(0xFF2f3438) : Color(0xFFedeeee),
+              ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset("assets/google.png", width: 6.w, height: 6.w),
+                SizedBox(width: 2.w),
+                Text(
+                  "Google",
+                  style: TextStyle(
+                    fontSize: 18.sp,
+                    fontWeight: FontWeight.w500,
+                    color: darkMode ? Colors.white : Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        SizedBox(width: 4.w),
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 4.w),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4.w),
+              border: Border.all(
+                width: 2,
+                color: darkMode ? Color(0xFF2f3438) : Color(0xFFedeeee),
+              ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset("assets/google.png", width: 6.w, height: 6.w),
+                SizedBox(width: 2.w),
+                Text(
+                  "Google",
+                  style: TextStyle(
+                    fontSize: 18.sp,
+                    fontWeight: FontWeight.w500,
+                    color: darkMode ? Colors.white : Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:queens/components/auth/others.dart';
 import 'package:queens/components/button.dart';
 import 'package:queens/components/checkBox.dart';
 import 'package:queens/components/textField.dart';
@@ -37,78 +38,128 @@ class _LoginState extends State<Login> {
     return Scaffold(
       backgroundColor: darkMode?Color(0xFF181e22):Colors.white,
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(4.w),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Center(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min, // Makes Column take minimum space vertically
-                  children: [
-                    Image.asset(
-                      darkMode ? 'assets/logo-dark.png' : 'assets/logo-light.png',
-                      width: 40.w,
-                      fit: BoxFit.contain,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(4.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min, // Makes Column take minimum space vertically
+                    children: [
+                      Image.asset(
+                        darkMode ? 'assets/logo-dark.png' : 'assets/logo-light.png',
+                        width: 40.w,
+                        fit: BoxFit.contain,
+                      ),
+                      SizedBox(height:6.h),
+                      Text("Sign in to your account",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18.sp,
+                          color: darkMode?Colors.white:Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 2.h,),
+                Text("Email",
+                  style: TextStyle(
+                    fontSize: 17.sp,
+                    fontWeight: FontWeight.w500,
+                    color: darkMode?Colors.white:Colors.black
+                  ),
+                ),
+                SizedBox(height: 1.h,),
+                CustomTextfield(
+                  controller: emailController,
+                  focusNode: emailFocusNode,
+                  validator: _validateEmail,
+                  hintText: "Enter your email",
+                  isPassword: false,
+                ),
+                SizedBox(height: 2.h,),
+                Text("Password",
+                  style: TextStyle(
+                      fontSize: 17.sp,
+                      fontWeight: FontWeight.w500,
+                      color: darkMode?Colors.white:Colors.black
+                  ),
+                ),
+                SizedBox(height: 1.h,),
+                CustomTextfield(
+                  controller: passwordController,
+                  focusNode: passwordFocusNode,
+                  validator: _validatePassword,
+                  hintText: "Enter your password",
+                  isPassword: true,
+                ),
+                SizedBox(height: 1.h,),
+                CustomCheckbox(),
+                SizedBox(height: 3.h,),
+                Button(title: "SIGN IN", textColor: Colors.white, bg: Color(0xFF0d5ef9), onTapCallback: (){}),
+                SizedBox(height: 3.h,),
+                Center(
+                  child: Text("Forgot Password?",
+                    style: TextStyle(
+                      fontSize: 16.sp,
+                      color: Color(0xFF0d5ef9)
                     ),
-                    SizedBox(height:6.h),
-                    Text("Sign in to your account",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 18.sp,
-                        color: darkMode?Colors.white:Colors.black,
+                  ),
+                ),
+                SizedBox(height: 2.h,),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        color: darkMode?Color(0xFF2f3438):Color(0xFFedeeee),
+                        thickness: 2,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Text(
+                        "or continue with",
+                        style: TextStyle(fontSize: 16,color: darkMode?Colors.white:Colors.black),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        color: darkMode?Color(0xFF2f3438):Color(0xFFedeeee),
+                        thickness: 2,
                       ),
                     ),
                   ],
                 ),
-              ),
-              SizedBox(height: 2.h,),
-              Text("Email",
-                style: TextStyle(
-                  fontSize: 17.sp,
-                  fontWeight: FontWeight.w500,
-                  color: darkMode?Colors.white:Colors.black
-                ),
-              ),
-              SizedBox(height: 1.h,),
-              CustomTextfield(
-                controller: emailController,
-                focusNode: emailFocusNode,
-                validator: _validateEmail,
-                hintText: "Enter your email",
-                isPassword: false,
-              ),
-              SizedBox(height: 2.h,),
-              Text("Password",
-                style: TextStyle(
-                    fontSize: 17.sp,
-                    fontWeight: FontWeight.w500,
-                    color: darkMode?Colors.white:Colors.black
-                ),
-              ),
-              SizedBox(height: 1.h,),
-              CustomTextfield(
-                controller: passwordController,
-                focusNode: passwordFocusNode,
-                validator: _validatePassword,
-                hintText: "Enter your password",
-                isPassword: true,
-              ),
-              SizedBox(height: 1.h,),
-              CustomCheckbox(),
-              SizedBox(height: 3.h,),
-              Button(title: "SIGN IN", textColor: Colors.white, bg: Color(0xFF0d5ef9), onTapCallback: (){}),
-              SizedBox(height: 3.h,),
-              Center(
-                child: Text("Forgot Password?",
-                  style: TextStyle(
-                    fontSize: 16.sp,
-                    color: Color(0xFF0d5ef9)
+                SizedBox(height: 2.h,),
+                Others(),
+                SizedBox(height: 2.h,),
+                Center(
+                  child: RichText(
+                      text:TextSpan(
+                        text: "Don't have an account? ",
+                        style: TextStyle(
+                          fontSize: 16.sp,
+                          color: darkMode?Colors.white:Colors.black
+                        ),
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: "Sign up",
+                            style: TextStyle(
+                              fontSize: 16.sp,
+                              color: Color(0xFF0d5ef9),
+                              fontWeight: FontWeight.bold
+                            ),
+                          ),
+                        ]
+                      )
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
