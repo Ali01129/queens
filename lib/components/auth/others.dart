12ae1df41aae1f7a2b207cbb04ec417e427edf43
Ali01129 <chaudharyalinawazz@gmail.com>
2025-07-07 21:30:06 +0500

    added google auth for android but not more than then like : going to bio is not added register and login not different: todo::after signup go to bio during login go to home

--- lib/components/auth/others.dart
+++ lib/components/auth/others.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
 class Others extends StatelessWidget {
@@ -10,34 +12,60 @@ class Others extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     bool darkMode = isDarkMode(context);
+
+    signInWithGoogle()async{
+
+      //begin google process
+      final GoogleSignInAccount? gUser=await GoogleSignIn().signIn();
+
+      if(gUser==null){
+        return null;
+      }
+
+      //obtain auth details from request
+      final GoogleSignInAuthentication gAuth= await gUser.authentication;
+
+      //create credentials for user
+      final credentials= GoogleAuthProvider.credential(
+        accessToken: gAuth.accessToken,
+        idToken: gAuth.idToken,
+      );
+
+      //finally we can signin
+      return FirebaseAuth.instance.signInWithCredential(credentials);
+    }
 
     return Row(
       children: [
         Expanded(
-          child: Container(
-            padding: EdgeInsets.symmetric(vertical: 4.w),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(4.w),
-              border: Border.all(
-                width: 2,
-                color: darkMode ? Color(0xFF2f3438) : Color(0xFFedeeee),
-              ),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset("assets/google.png", width: 6.w, height: 6.w),
-                SizedBox(width: 2.w),
-                Text(
-                  "Google",
-                  style: TextStyle(
-                    fontSize: 18.sp,
-                    fontWeight: FontWeight.w500,
-                    color: darkMode ? Colors.white : Colors.black,
-                  ),
+          child: GestureDetector(
+            onTap: signInWithGoogle,
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: 4.w),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4.w),
+                border: Border.all(
+                  width: 2,
+                  color: darkMode ? Color(0xFF2f3438) : Color(0xFFedeeee),
                 ),
-              ],
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset("assets/google.png", width: 6.w, height: 6.w),
+                  SizedBox(width: 2.w),
+                  Text(
+                    "Google",
+                    style: TextStyle(
+                      fontSize: 18.sp,
+                      fontWeight: FontWeight.w500,
+                      color: darkMode ? Colors.white : Colors.black,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
