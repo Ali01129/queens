ed78e86c9893a489a4a9bdaa9b17440c7c490a17
Ali01129 <chaudharyalinawazz@gmail.com>
2025-07-11 00:44:07 +0500

    google auth done for android not for ios

--- lib/components/auth/others.dart
+++ lib/components/auth/others.dart
@@ -1,7 +1,14 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:provider/provider.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
+
+import '../../database/userData.dart';
+import '../../provider/addressProvider.dart';
+import '../../provider/cartProvider.dart';
+import '../../provider/myOrdresProvider.dart';
+import '../../provider/userProvider.dart';
 
 class Others extends StatelessWidget {
   const Others({super.key});
@@ -15,33 +22,53 @@ class Others extends StatelessWidget {
 
     bool darkMode = isDarkMode(context);
 
-    signInWithGoogle()async{
+    Future<void> signInWithGoogle(BuildContext context) async {
+      try {
+        // Begin Google Sign-In
+        final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
+        if (gUser == null) {
+          return; // User canceled the sign-in
+        }
+        // Obtain auth details
+        final GoogleSignInAuthentication gAuth = await gUser.authentication;
+        // Create credentials
+        final AuthCredential credential = GoogleAuthProvider.credential(
+          accessToken: gAuth.accessToken,
+          idToken: gAuth.idToken,
+        );
 
-      //begin google process
-      final GoogleSignInAccount? gUser=await GoogleSignIn().signIn();
+        // Sign in with Firebase
+        final UserCredential userCredential =
+        await FirebaseAuth.instance.signInWithCredential(credential);
 
-      if(gUser==null){
-        return null;
+        final bool isNewUser = userCredential.additionalUserInfo?.isNewUser ?? false;
+
+        if (isNewUser) {
+          // Navigate to bio (new user)
+          Navigator.pushNamedAndRemoveUntil(context, '/bio', (route) => false);
+        } else {
+          // Navigate to home (existing user)
+          Navigator.pushNamedAndRemoveUntil(context, '/bottom', (route) => false);
+          ///adding data in provider
+          final userValue = Provider.of<UserProvider>(context, listen: false);
+          final data = await UserData().getUserData();
+          UserModel _user=UserModel(date: data?['dateOfBirth'],name: data?['name'], image: data?['imageUrl'], gender: data?['gender'], phone: data?['phoneNumber'], email:data?['email']);
+          userValue.setUser(_user);
+          Provider.of<CartProvider>(context, listen: false).setCart();
+          Provider.of<Myordresprovider>(context,listen:false).setMyOrders();
+          Provider.of<AddressProvider>(context, listen: false).setAddresses();
+        }
+      } catch (e) {
+        print('Google Sign-In error: $e');
+        // Optionally show an error message to user
       }
-
-      //obtain auth details from request
-      final GoogleSignInAuthentication gAuth= await gUser.authentication;
-
-      //create credentials for user
-      final credentials= GoogleAuthProvider.credential(
-        accessToken: gAuth.accessToken,
-        idToken: gAuth.idToken,
-      );
-
-      //finally we can signin
-      return FirebaseAuth.instance.signInWithCredential(credentials);
     }
 
     return Row(
       children: [
         Expanded(
           child: GestureDetector(
-            onTap: signInWithGoogle,
+            onTap: () => signInWithGoogle(context),
             child: Container(
               padding: EdgeInsets.symmetric(vertical: 4.w),
               decoration: BoxDecoration(
